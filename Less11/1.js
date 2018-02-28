f9d9e42119b7c05380b90a3acbffe777cc2513c4
shalyayura <stallker9696@gmail.com>
2018-02-28 15:57:21 +0200

    slider timer

--- Less11/1.js
+++ Less11/1.js
@@ -1,11 +1,19 @@
-document.getElementById('slider-left').onclick = sliderLeft;
+// document.getElementById('slider-left').onclick = sliderLeft;
+autoSlider();
 var left = 0;
+var timer;
 
-function sliderLeft(){
-	var polosa = document.getElementById('polosa');
-	left = left - 128;
-	if (left < -512) {
-		left = 0;
-	}
-	polosa.style.left = left +'px';
+
+function autoSlider (){
+	timer = setTimeout(function (){
+		var polosa = document.getElementById('polosa');
+		left = left - 128;
+		if (left < -512) {
+			left = 0;
+			clearTimeout(timer);
+		}
+		polosa.style.left = left +'px';
+		autoSlider ();
+	}, 1000);
 }
+
