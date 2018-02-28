8c4603f882c0e497f60320278a5e14d13b959de5
shalyayura <stallker9696@gmail.com>
2018-02-28 20:31:17 +0200

    Work with elements and classes

---
+++ Less12/1.js
@@ -0,0 +1,21 @@
+var p = document.getElementsByTagName('p');
+var one = document.getElementsByClassName('one');
+console.log(p);
+		//p.onclick = f1; 
+		for (var i=0; i<p.length; i++){
+			p[i].onclick = f1; 
+		}
+
+		for (var i=0; i<one.length; i++){
+			one[i].onclick = f2; 
+		}
+		 
+	function f1(){
+			//alert('Hello');
+			console.log(this);
+			this.style.background = 'pink';
+	}
+
+	function f2 (){
+		this.style.fontWeight = 'bold';
+	}
